47de9989fb337f87275e597f453315d1b68abc71
caijw <285025525@qq.com>
2020-03-30 17:51:08 +0800

    feat: add hello lib

--- binder/lib/binder.dart
+++ binder/lib/binder.dart
@@ -1,12 +1,30 @@
 import 'dart:async';
-
 import 'package:flutter/services.dart';
+import 'dart:ffi' as ffi;
+import 'dart:io' show Platform;
+
+// FFI signature of the hello_world C function
+typedef hello_world_func = ffi.Void Function();
+// Dart type definition for calling the C foreign function
+typedef HelloWorld = void Function();
+
 
 class Binder {
+  static ffi.DynamicLibrary _dylib;
   static const MethodChannel _channel =
       const MethodChannel('binder');
 
   static Future<String> get platformVersion async {
+    if (_dylib == null) {
+      var path = "../hello_library/libhello.so";
+      if (Platform.isMacOS) path = '../hello_library/libhello.dylib';
+      _dylib = ffi.DynamicLibrary.open(path);
+    }
+    // Look up the C function 'hello_world'
+    final HelloWorld hello = _dylib
+        .lookup<ffi.NativeFunction<hello_world_func>>('hello_world')
+        .asFunction();
+    hello();
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
