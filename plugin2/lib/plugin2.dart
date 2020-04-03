8a049920199cf0937b8226655f4e386ab4112375
caijw <285025525@qq.com>
2020-04-03 23:56:41 +0800

    update

--- plugin2/lib/plugin2.dart
+++ plugin2/lib/plugin2.dart
@@ -3,12 +3,13 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class Plugin2 {
-  static const MethodChannel _channel =
+  // static const MethodChannel _channel =
       // const MethodChannel('WeOS/IPC');
-      const MethodChannel('plugin2');
+      // const MethodChannel('plugin2');
 
   static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
+    String version = '';
+    // final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 }
