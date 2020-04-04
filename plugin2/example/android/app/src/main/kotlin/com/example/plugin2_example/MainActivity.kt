9ce9eee0408610933678516ba0ac0d43e77b2b2b
caijw <285025525@qq.com>
2020-04-04 21:48:10 +0800

    feat: update

--- plugin2/example/android/app/src/main/kotlin/com/example/plugin2_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.plugin2_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- plugin2/ios/Classes/Plugin2Plugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface Plugin2Plugin : NSObject<FlutterPlugin>
-@end
