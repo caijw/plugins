4581b03745a73d1513bf8e7ea925e53fd8536822
caijw <285025525@qq.com>
2020-04-04 23:43:00 +0800

    feat: update

--- ipc_extension/lib/ipc_extension.dart
+++ ipc_extension/lib/ipc_extension.dart
@@ -1,7 +1,12 @@
 library ipc_extension;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart-ext:ipc_extension';
+
+void setDesktopWindow(String title) native "setDesktopWindow";
+void setDrawerWindow(String title) native "setDrawerWindow";
+void setWindowSource(String title, int x, int y) native "setWindowSource";
+int flush() native "flush";
